6c872d3eee1cbd8cb5916d6d11df9d33672dbe93
Freek Berson <freek.berson@wortell.nl>
2019-10-23 15:28:57 +0200

    Change ProgressPreference to Silent

--- SCCM-PS-Extension.ps1
+++ SCCM-PS-Extension.ps1
@@ -35,6 +35,7 @@ function log
 Set-MpPreference -DisableRealtimeMonitoring $true
 
 #GetSCCMISO
+$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri "https://pmmazurelabfiles.blob.core.windows.net/pmmaurelabfilescontainer/mu_system_center_configuration_manager_current_branch_version_1802_x86_x64_dvd_12064903.iso" -OutFile "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.9.5\Downloads\0\mu_system_center_configuration_manager_current_branch_version_1802_x86_x64_dvd_12064903.iso"
 
 #Enable CredSSP to allow Multiple HOP Remote PowerShell
